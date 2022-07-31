07ad0e65a04566b733d0983ac7e3f9042f9afa42
wulitong <18212404701@163.com>
2022-07-31 16:46:31 +0800

    test

--- http-tools/src/main/java/com/wltt/http/BdTest.java
+++ http-tools/src/main/java/com/wltt/http/BdTest.java
@@ -9,6 +9,7 @@ import cn.hutool.http.HttpUtil;
  */
 public class BdTest {
     public static void main(String[] args) {
-        String resStr = HttpUtil.post("www.baidu.com","test");
+        String resStr = HttpUtil.post("http://factory.sit01.jaalantech.com/api/0001/accrual/group/list?id=AM202112021332442805642365172545","test");
+        System.out.println(resStr);
     }
 }
